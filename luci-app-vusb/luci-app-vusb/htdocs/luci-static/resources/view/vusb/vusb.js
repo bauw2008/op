db712653e7d0cd1d684a065a5b304b1a66a7f436
bauw2008 <bauw@msn.com>
2024-04-20 05:03:31 -0700

    Initial commit

--- luci-app-vusb/luci-app-vusb/htdocs/luci-static/resources/view/vusb/vusb.js
+++ luci-app-vusb/luci-app-vusb/htdocs/luci-static/resources/view/vusb/vusb.js
@@ -19,7 +19,7 @@ function getServiceStatus() {
     return L.resolveDefault(callServiceList('vusb'), {}).then(function (res) {
         var isRunning = false;
         try {
-            isRunning = res['vusb']['instances']['vusb']['running'];
+            isRunning = res['vusb']['instances']['instance1']['running'];
         } catch (e) { }
         return isRunning;
     });
@@ -30,14 +30,19 @@ function renderStatus(isRunning) {
     var spanTemp = '<em><span style="color:%s"><strong>%s %s</strong></span></em>';
     var renderHTML;
     if (isRunning) {
-        renderHTML = spanTemp.format('green', _('Vusb'), _('RUNNING'));
+        renderHTML = spanTemp.format('green', _('VirtualHere USB Server'), _('RUNNING'));
     } else {
-        renderHTML = spanTemp.format('red', _('Vusb'), _('NOT RUNNING'));
+        renderHTML = spanTemp.format('red', _('VirtualHere USB Server'), _('NOT RUNNING'));
     }
     return renderHTML;
 }
 
+
 return view.extend({
+	load: function() {
+		return uci.load('vusb');
+	},
+	
     render: function() {
 		var m, s, o;
         m = new form.Map('vusb', _('VirtualHere USB Server'), _('配置虚拟USB服务。将网络设备与本地主机上的USB设备进行映射。<a id="client-software-link" href="http://www.virtualhere.com/usb_client_software">客户端下载</a>，并根据本地主机架构进行安装。<br>注册码（x86、arm64无效）：xxxxxxxxxxxx，999，MCACDkn0jww6R5WOIjFqU/apAg4Um+mDkU2TBcC7fA1FrA=='));
@@ -64,24 +69,23 @@ return view.extend({
         o = s.option(form.Flag, 'enabled', _('启用'));
         o.default = o.disabled;
         o.rmempty = false;
-        
-        // 添加外网访问开关选项
-        o = s.option(form.Flag, 'access', _('外网访问-firewall4'));
-        o.default = o.disabled;
-        o.rmempty = false;
-
-        // 添加端口号设置选项
-        o = s.option(form.Value, 'port', _('端口'));
-        o.default = '7575'; // 默认端口号为7575
-        o.placeholder = '请输入端口号'; // 输入框的占位符
-        
+		
+		// 添加端口号设置选项
+        // o = s.option(form.Value, 'port', _('端口'));
+        // o.default = '7575'; // 默认端口号为7575
+        // o.placeholder = '请输入端口号'; // 输入框的占位符
+  
         // 添加IPv6接口开关选项
-        o = s.option(form.Flag, 'ipv6', _('启用 IPv6'));
+        o = s.option(form.Flag, 'ipv6', _('IPv6'));
         o.default = o.disabled;
         o.rmempty = false;
+		
+		// 添加外网访问开关选项
+        // o = s.option(form.Flag, 'web', _('外网访问-firewall4'));
+        // o.default = o.disabled;
+        // o.rmempty = false;
 
         // 返回渲染结果
         return m.render();
     }
 });
-
